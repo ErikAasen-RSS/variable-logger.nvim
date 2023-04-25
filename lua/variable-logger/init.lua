8ff56e9510851e17414ca8e485dcd784dd900595
Erik <ewaasen@ucdavis.edu>
2023-04-25 09:51:37 -0700

    wip

--- lua/variable-logger/init.lua
+++ lua/variable-logger/init.lua
@@ -1,10 +1,22 @@
-local function log_variable()
+local default_config = {
+  prefix = {},
+}
+
+local M = {}
+
+function M.setup(opt)
+  if opt ~= nil then
+    M.config = opt
+  end
+end
+
+function M.log_variable(prefix)
   vim.fn.setreg('"', "")
   local current_position = vim.api.nvim_win_get_cursor(0)
   vim.cmd("normal! viwy")
   local yanked_text = vim.fn.getreg('"')
-  vim.fn.setreg('"', string.format("console.log('%s', %s)\n", yanked_text, yanked_text))
+  vim.fn.setreg('"', string.format("console.log(%s, %s, '%s', %s)\n", M.config.prefix, prefix, yanked_text, yanked_text))
   vim.api.nvim_win_set_cursor(0, current_position)
 end
 
-return log_variable
+return M
